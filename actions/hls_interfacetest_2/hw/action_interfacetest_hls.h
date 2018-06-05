d5dd142aae20e526139e9dfd27f3d9c6d4e8f9d5
xyangbj <xyangbj@cn.ibm.com>
2018-06-05 09:59:13 +0800

    upload hls_interfacetest_2 for directly read/write data from memory

---
+++ actions/hls_interfacetest_2/hw/action_interfacetest_hls.h
@@ -0,0 +1,39 @@
+#ifndef __ACTION_INTERFACETEST_HLS_H__
+#define __ACTION_INTERFACETEST_HLS_H__
+
+/*
+ * Copyright 2017 International Business Machines
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <stdint.h>
+#include <string.h>
+#include <ap_int.h>
+#include <hls_stream.h>
+
+#include <hls_snap.H>
+#include <action_interfacetest.h>
+
+#define CARD_DRAM_SIZE (1 * 1024 *1024 * 1024)
+#define MAX_NB_OF_BYTES_READ  (4 * 1024)
+#define MAX_NB_OF_WORDS_READ MAX_NB_OF_BYTES_READ/BPERDW
+#define BPERCL 128                           //128Bytes for one PSL Cacheline
+//---------------------------------------------------------------------
+typedef struct {
+    CONTROL Control;       /*  16 bytes */
+    interfacetest_job_t Data;     /* 108 Bytes */
+    uint8_t padding[SNAP_HLS_JOBSIZE - sizeof(interfacetest_job_t)];
+} action_reg;
+
+#endif  /* __ACTION_MC_HLS_H__ */
